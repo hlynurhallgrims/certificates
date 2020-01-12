d5c48d992be8831d67130f2f5646227801896b3e
hlynurhallgrims <hlynurh@gmail.com>
2020-01-12 06:06:12 +0000

    Einum Harvard kúrsi og einum DataCamp kúrsi bætt við

--- combine_pdfs.R
+++ combine_pdfs.R
@@ -2,8 +2,11 @@ library(pdftools)
 library(fs)
 library(tidyverse)
 
-pdf_combine(fs::dir_ls(here::here("pdfs", "datacamp_courses")))
+pdf_combine(fs::dir_ls(here::here("pdfs", "datacamp_courses")), 
+            output = here::here("pdfs", "Hlynur_Datacamp_kursar.pdf"))
 
-pdf_combine(fs::dir_ls(here::here("pdfs", "datacamp_tracks")))
+pdf_combine(fs::dir_ls(here::here("pdfs", "datacamp_tracks")),
+            output = here::here("pdfs", "Hlynur_Datacamp_namslinur.pdf"))
 
-pdf_combine(fs::dir_ls(here::here("pdfs", "harvard_stanford_jh")))
+pdf_combine(fs::dir_ls(here::here("pdfs", "harvard_stanford_jh")),
+            output = here::here("pdfs", "Hlynur_Harvard_Stanford_JohnsHopkins_kursar.pdf"))
